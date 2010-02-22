cffbcaa481b90805320208f5c32252390650e14a
Arvid Norlander <code@vorpal.se>
2010-02-22 17:21:10 +0100

    Merge back fingerprint maintainence scripts from supervisor-trunk.

--- tools/gen_fprint_list.sh
+++ tools/gen_fprint_list.sh
@@ -3,7 +3,7 @@
 ###########################################################################
 #                                                                         #
 #  efunge - a Befunge-98 interpreter in Erlang.                           #
-#  Copyright (C) 2008-2009  Arvid Norlander                               #
+#  Copyright (C) 2008-2010  Arvid Norlander                               #
 #                                                                         #
 #  This program is free software: you can redistribute it and/or modify   #
 #  it under the terms of the GNU General Public License as published by   #
@@ -23,72 +23,48 @@
 ###########################################################################
 
 # Generate the fingerprint list
+# This must be run from top source directory.
 
-# Check bash version. We need at least 3.1
-# Lets not use anything like =~ here because
-# that may not work on old bash versions.
-if [[ "${BASH_VERSINFO[0]}${BASH_VERSINFO[1]}" -lt 31 ]]; then
+# Error to fail with for old bash.
+fail_old_bash() {
 	echo "Sorry your bash version is too old!"
-	echo "You need at least version 3.1 of bash."
+	echo "You need at least version 3.2.10 of bash"
 	echo "Please install a newer version:"
-	echo " * Either use your distro's packages."
+	echo " * Either use your distro's packages"
 	echo " * Or see http://www.gnu.org/software/bash/"
 	exit 2
+}
+
+# Check bash version. We need at least 3.2.10
+# Lets not use anything like =~ here because
+# that may not work on old bash versions.
+if [[ "${BASH_VERSINFO[0]}${BASH_VERSINFO[1]}" -lt 32 ]]; then
+	fail_old_bash
+elif [[ "${BASH_VERSINFO[0]}${BASH_VERSINFO[1]}" -eq 32 && "${BASH_VERSINFO[2]}" -lt 10 ]]; then
+	fail_old_bash
+fi
+
+if [[ ! -d src/fingerprints ]]; then
+	echo "ERROR: Run from top source directory please." >&2
+	exit 1
 fi
 
 set -e
 
-# This must be run from top source directory.
-
-die() {
-	echo "ERROR: $1" >&2
+if [[ ! -f tools/fprint_funcs.sh ]]; then
+	echo "ERROR: Couldn't find tools/fprint_funcs.sh." >&2
 	exit 1
-}
-
-progress() {
-	echo " * ${1}..."
-}
-progresslvl2() {
-	echo "   * ${1}..."
-}
-
-status() {
-	echo "   ${1}"
-}
-statuslvl2() {
-	echo "     ${1}"
-}
-
-
-# Char to decimal
-ord() {
-	printf -v "$1" '%d' "'$2"
-}
-
-
-if [[ ! -d src/fingerprints ]]; then
-	die "Run from top source directory please."
+fi
+source tools/fprint_funcs.sh
+if [[ $? -ne 0 ]]; then
+	echo "ERROR: Couldn't load tools/fprint_funcs.sh." >&2
+	exit 1
 fi
 
 addtolist() {
 	echo "$1" >> "../efunge_fingerindex.erl"
 }
 
-
-# $1 is fingerprint name
-# Returns if ok, otherwise it dies.
-checkfprint() {
-	local FPRINT="$1"
-	if [[ $FPRINT =~ ^[A-Z0-9]{4}$ ]]; then
-		status "Fingerprint name $FPRINT ok style."
-	elif [[ $FPRINT =~ ^[^\ /\\]{4}$ ]]; then
-		status "Fingerprint name $FPRINT probably ok (but not common style)."
-		status "Make sure each char is in the ASCII range 0-254."
-		status "Note that alphanumeric (upper case only) fingerprint names are strongly preferred."
-	else
-		die "Not valid format for fingerprint name."
-	fi
-}
 
 # This is to allow sorted list even with aliases...
 # I hate aliases...
@@ -101,14 +77,10 @@ genfprintinfo() {
 	local FPRINT="$1"
 
 	# Variables
-	local URL=""
-	local F108_URI="NULL"
-	local SAFE=""
-	local OPCODES=""
-	local DESCRIPTION=""
-	local MYALIASES=()
-
-	local OPCODES=""
+	local fp_URL fp_SAFE fp_OPCODES fp_DESCRIPTION
+	local fp_F109_URI fp_CONDITION
+	local fp_ALIASES
+	local fp_OPCODE_NAMES fp_OPCODE_DESC
 
 	progress "Processing $FPRINT"
 
@@ -125,111 +97,11 @@ genfprintinfo() {
 	else
 		die "Sorry you need a spec file for the fingerprint. It should be placed at src/fingerprints/${FPRINT}.spec"
 	fi
+	parse_spec "${FPRINT}"
 
-
-	progresslvl2 "Parsing spec file"
-	IFS=$'\n'
-	local line type data instr name desc number
-	# First line is %fingerprint-spec 1.2
-	exec 4<"${FPRINT}.spec"
-	read -ru 4 line
-	if [[ "$line" != "%fingerprint-spec 1.2" ]]; then
-		die "Either the spec file is not a fingerprint spec, or it is not version 1.2 of the format."
-	fi
-
-	# 0: pre-"begin instrs"
-	# 1: "begin-instrs"
-	local parsestate=0
-
-
-	while read -ru 4 line; do
-		if [[ "$line" =~ ^# ]]; then
-			continue
-		fi
-		if [[ $parsestate == 0 ]]; then
-			IFS=':' read -rd $'\n' type data <<< "$line" || true
-			case $type in
-				"%fprint")
-					if [[ "$FPRINT" != "$data" ]]; then
-						die "fprint is spec file doesn't match."
-					fi
-					;;
-				"%url")
-					URL="$data"
-					;;
-				"%f108-uri")
-					F108_URI="\"$data\""
-					;;
-				"%alias")
-					MYALIASES+=( "$data" )
-					;;
-				"%desc")
-					DESCRIPTION="$data"
-					;;
-				"%safe")
-					SAFE="$data"
-					;;
-				"%begin-instrs")
-					parsestate=1
-					;;
-				"#"*)
-					# A comment, ignore
-					;;
-				*)
-					die "Unknown entry $type found in ${FPRINT}."
-					;;
-			esac
-		else
-			if [[ "$line" == "%end" ]]; then
-				break
-			fi
-			# Parse instruction lines.
-			IFS=$'\t' read -rd $'\n' instr name desc <<< "$line"
-
-			OPCODES+="$instr"
-		fi
-	done
-
-	unset IFS
-
-	statuslvl2 "Done parsing."
-
-	exec 4<&-
-
-	progresslvl2 "Validating the parsed data"
-
-	if [[ "$URL" ]]; then
-		statuslvl2 "%url: Good, not empty"
-	else
-		die "%url is not given or is empty."
-	fi
-
-	if [[ "$DESCRIPTION" ]]; then
-		statuslvl2 "%desc: Good, not empty"
-	else
-		die "%desc is not given or is empty."
-	fi
-
-	if [[ ( "$SAFE" == "true" ) || ( "$SAFE" == "false" ) ]]; then
-		statuslvl2 "%safe: OK"
-	else
-		die "%safe must be either true or false."
-	fi
-
-	if [[ "$OPCODES" =~ ^[A-Z]+$ ]]; then
-		# Check that they are sorted.
-		local previousnr=0
-		for (( i = 0; i < ${#OPCODES}; i++ )); do
-			ord number "${OPCODES:$i:1}"
-			if [[ $previousnr -ge $number ]]; then
-				die "Instructions not sorted or there are duplicates"
-			else
-				previousnr=$number
-			fi
-		done
-		statuslvl2 "Instructions: OK"
-	else
-		die "The opcodes are not valid. The must be in the range A-Z"
+	# Check for unsupported features.
+	if [[ $fp_CONDITION ]]; then
+		die "Sorry, efunge doesn't support %condition"
 	fi
 
 	progresslvl2 "Generating data for list"
@@ -241,13 +113,13 @@ genfprintinfo() {
 	FPRINTERLANGHEX="16#$FPRINTHEX"
 	FPRINTHEX="0x$FPRINTHEX"
 	ENTRIES+=( "$FPRINTHEX" )
-	ENTRYL1[$FPRINTHEX]="%% ${FPRINT} - ${DESCRIPTION}"
-	ENTRYL2[$FPRINTHEX]="lookup(${FPRINTERLANGHEX})  -> { \"${OPCODES}\", fun fing${FPRINT}:load/1 };"
+	ENTRYL1[$FPRINTHEX]="%% ${FPRINT} - ${fp_DESCRIPTION}"
+	ENTRYL2[$FPRINTHEX]="lookup(${FPRINTERLANGHEX})  -> { \"${fp_OPCODES}\", fun fing${FPRINT}:load/1 };"
 	statuslvl2 "Done"
-	if [[ ${!MYALIASES[*]} ]]; then
+	if [[ ${!fp_ALIASES[*]} ]]; then
 		progresslvl2 "Generating aliases..."
 		local myalias
-		for myalias in "${MYALIASES[@]}"; do
+		for myalias in "${fp_ALIASES[@]}"; do
 			checkfprint "$myalias"
 			local ALIASHEX ALIASERLANGHEX
 			for (( i = 0; i < ${#myalias}; i++ )); do
@@ -258,8 +130,8 @@ genfprintinfo() {
 			ALIASHEX="0x$ALIASHEX"
 
 			ENTRIES+=("$ALIASHEX")
-			ENTRYL1[$ALIASHEX]="%% ${myalias} - Alias for ${FPRINT} - ${DESCRIPTION}"
-			ENTRYL2[$ALIASHEX]="lookup(${ALIASERLANGHEX})  -> { \"${OPCODES}\", fun fing${FPRINT}:load/1 };"
+			ENTRYL1[$ALIASHEX]="%% ${myalias} - Alias for ${FPRINT} - ${fp_DESCRIPTION}"
+			ENTRYL2[$ALIASHEX]="lookup(${ALIASERLANGHEX})  -> { \"${fp_OPCODES}\", fun fing${FPRINT}:load/1 };"
 		done
 	fi
 }
@@ -279,7 +151,7 @@ progress "Creating list file"
 cat > "../efunge_fingerindex.erl" << EOF
 %%%----------------------------------------------------------------------
 %%% efunge - a Befunge-98 interpreter in Erlang.
-%%% Copyright (C) 2008 Arvid Norlander <anmaster AT tele2 DOT se>
+%%% Copyright (C) 2008-2010 Arvid Norlander <anmaster AT tele2 DOT se>
 %%%
 %%% This program is free software: you can redistribute it and/or modify
 %%% it under the terms of the GNU General Public License as published by
